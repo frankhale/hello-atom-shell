5e7b8f463d77d5fa360a83ea70fae4af11ce3ef3
Frank Hale <frankhale@gmail.com>
2015-02-01 13:33:22 -0500

    Update CLJS and CLJSBUILD

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject hello-world "0.1.0-SNAPSHOT"
+(defproject hello-world "0.2.0-SNAPSHOT"
   :description "A hello, world application for atom-shell"
   :url "http://github.com/frankhale/hello-atom-shell"
-    :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2202"]]
-  :plugins [[lein-cljsbuild "1.0.3"]]
+    :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2740"]]
+  :plugins [[lein-cljsbuild "1.0.4"]]
   :cljsbuild
 	{:builds
 		[{:source-paths ["src"],
